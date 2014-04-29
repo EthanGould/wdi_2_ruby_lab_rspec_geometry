440964edef047b7b133cc971e7e96f43143dabb3
Ethan Gould <ethan.g.907@gmail.com>
2014-04-29 13:02:31 -0400

    changed spec descritions to be more correct

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,16 @@ class Triangle
     a + b + c
   end
 
-  def halfed
-    0.5 * perimeter
-  end
-
   def area
     Math.sqrt(halfed * (halfed - a) * (halfed - b) * (halfed - c))
   end
 
   def valid?
     c < a + b ? true : false
+  end
+
+  private
+  def halfed
+    0.5 * perimeter
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -7,19 +7,19 @@ describe Triangle do
     @second_triangle = Triangle.new(a: 7, b: 5, c: 10)
     @third_triangle = Triangle.new(a: 8, b: 7, c: 20)
   end
-  it 'should calculate the perimeter of the triangle' do
+  it 'calculates the perimeter of the triangle' do
     expect(@triangle.perimeter).to eq 30
 
     expect(@second_triangle.perimeter).to eq 22
   end
 
-  it 'should calculate the area of the triangle' do
+  it 'calculates the area of the triangle' do
     expect(@triangle.area).to be_within(0.01).of (43.30)
 
     expect(@second_triangle.area).to be_within(0.01).of (16.25)
   end
 
-  it 'should check the triangel for validity' do
+  it 'checks the triangle for validity' do
     expect(@triangle.valid?).to eq true
 
     expect(@third_triangle.valid?).to eq false
